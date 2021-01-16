1c51d4687aa0e1a530fdb4ecec1721bee9be941f
keys4wordg <keys4wordg@gmail.com>
2021-01-16 17:44:52 +0300

    change structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,62 @@
-from flask import Flask, render_template, redirect, url_for, request
+from datetime import datetime
+import os
+
+from flask import Flask
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user, fresh_login_required
+from flask_migrate import Migrate, MigrateCommand
+from flask_user import UserManager
+from flask_wtf.csrf import CSRFProtect
 
 
-app = Flask(__name__)
-app.config.from_pyfile('config.cfg')
-login_manager = LoginManager(app)
-db = SQLAlchemy(app)
+# Instantiate Flask extensions
+csrf_protect = CSRFProtect()
+db = SQLAlchemy()
+migrate = Migrate()
+
+# Initialize Flask Application
+def create_app(extra_config_settings={}):
+    """Create a Flask application.
+    """
+    # Instantiate Flask
+    app = Flask(__name__)
+
+    # Load common settings
+    app.config.from_pyfile('config.cfg')
+
+    # Setup Flask-SQLAlchemy
+    db.init_app(app)
+
+    # Setup Flask-Migrate
+    migrate.init_app(app, db)
+
+    # Setup WTForms CSRFProtect
+    csrf_protect.init_app(app)
+
+    # Register blueprints
+    from .views import register_blueprints
+    register_blueprints(app)
+
+    # Define bootstrap_is_hidden_field for flask-bootstrap's bootstrap_wtf.html
+    from wtforms.fields import HiddenField
+
+    def is_hidden_field_filter(field):
+        return isinstance(field, HiddenField)
+
+    app.jinja_env.globals['bootstrap_is_hidden_field'] = is_hidden_field_filter
+
+    # Setup Flask-User to handle user account related forms
+    from .models.user_models import User
+    from .views.main_views import user_profile_page
+
+    # Setup Flask-User
+    user_manager = UserManager(app, db, User)
+
+    @app.context_processor
+    def context_processor():
+        return dict(user_manager=user_manager)
+
+    return app
 
 
 ###############################
