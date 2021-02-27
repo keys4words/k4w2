62ced9b6295ed10fcb681d65ce127fa1a1682079
keys4words <keys4words@gmail.com>
2021-02-27 15:03:29 +0300

    add gunicorn

--- config.py
+++ config.py
@@ -12,17 +12,17 @@ class BaseConfig():
 class DevConfig(BaseConfig):
     FLASK_ENV = 'development'
     DEBUG = True
-    SECRET_KEY = 'something-ultimately-secret'
-
+    SECRET_KEY = 'asldkjfiu4304jfajfjPJF_DJ$EJsdfadf43343s'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_password@db-postgres:5432/flask-deploy'
     CELERY_BROKER = 'pyamqp://rabbit_user:rabbit_password@broker-rabbitmq//'
     CELERY_RESULT_BACKEND = 'rpc://rabbit_user:rabbit_password@broker-rabbitmq//'
 
 
 class ProductionConfig(BaseConfig):
     FLASK_ENV = 'production'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_password@db-postgres:5432/flask-deploy'
-    CELERY_BROKER = 'pyamqp://rabbit_user:rabbit_password@broker-rabbitmq//'
-    CELERY_RESULT_BACKEND = 'rpc://rabbit_user:rabbit_password@broker-rabbitmq//'
+    DEBUG = False
+    SECRET_KEY = 'asldkjfiu4304jfajfjPJF_DJ$EJsdfadf43343s'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app', 'db.sqlite')
 
 
 class TestConfig(BaseConfig):
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from app import create_app
 
 
-create_app().run()
+app = create_app()
